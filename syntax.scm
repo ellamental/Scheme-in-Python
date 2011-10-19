d97857d1846ed2bc2255b122c2ab94e46619196f
Nick Zarczynski <nick@nickzarr.com>
2011-10-18 22:50:49 -0500

    syntax: added define and if

---
+++ syntax.scm
@@ -0,0 +1,12 @@
+(scheme-syntax define "
+def f(expr):
+  frame[expr.car] = scheme_eval(expr.cdr.car)
+")
+
+(scheme-syntax if "
+def f(expr):
+  if scheme_eval(expr.car):
+    return scheme_eval(expr.cdr.car)
+  else:
+    return scheme_eval(expr.cdr.cdr.car)
+")
